6d9f09a42cbb9c45fa8d95593c7fa7a699bfbccf
Varsha Pyata <varshapyata18@gmail.com>
2026-01-04 18:31:54 +0530

    Add mockPrices data for delivery platforms

---
+++ frontend/src/data/mockprices.js
@@ -0,0 +1,29 @@
+export const mockPrices = [
+  {
+    platform: "Blinkit",
+    price: 52,
+    quantity: "1L",
+    delivery: 10,
+    fee: 3,
+    total: 65,
+    time: "15 mins"
+  },
+  {
+    platform: "Zepto",
+    price: 50,
+    quantity: "1L",
+    delivery: 15,
+    fee: 5,
+    total: 70,
+    time: "10 mins"
+  },
+  {
+    platform: "Swiggy Instamart",
+    price: 55,
+    quantity: "1L",
+    delivery: 0,
+    fee: 6,
+    total: 61,
+    time: "25 mins"
+  }
+];
